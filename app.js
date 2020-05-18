f99574c780bc611f43e95df897341fae233c6ad4
darshan-km <darshan.km13@gmail.com>
2020-05-18 17:03:07 +0530

    updated changes

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-// // Init Github
-// const github = new Github;
-// // Init UI
-// const ui = new UI;
-
-// // Search input
-// const searchUser = document.getElementById('searchUser');
-
-// // Search input event listener
-// searchUser.addEventListener('keyup', (e) => {
-//   // Get input text
-//   const userText = e.target.value;
-
-//   if(userText !== ''){
-//    // Make http call
-//    github.getUser(userText)
-//     .then(data => {
-//       if(data.profile.message === 'Not Found') {
-//         // Show alert
-//         ui.showAlert('User not found', 'alert alert-danger');
-//       } else {
-//         // Show profile
-//         ui.showProfile(data.profile);
-//         ui.showRepos(data.repos);
-//       }
-//     })
-//   } else {
-//     // Clear profile
-//     ui.clearProfile();
-//   }
-// }); 
-
 let getGit = new Github;
 let ui = new UI;
 
@@ -42,12 +10,16 @@ const searchValue = e.target.value;
 if(searchValue.length !== 0 || searchValue !== null || searchUser !== undefined){
   getGit.getUser(searchValue)
   .then((data) => {
-    if(data.profile.message === 'Not Found'){
+    if(data.profile.message === "Not Found"){
       //show not found alert
+      ui.showAlert('This user doest not exist', 'alert alert-danger');
     }else{
     //show data
       ui.showProfile(data.profile);
+      ui.showRepos(data.repos);
     }
   })
+}else{
+  ui.clearProfile();
 }
 });
--- ui.js
+++ ui.js
@@ -1,100 +1,3 @@
-// class UI {
-//   constructor() {
-//     this.profile = document.getElementById('profile');
-//   }
-
-//   // Display profile in UI
-//   showProfile(user) {
-//     this.profile.innerHTML = `
-//       <div class="card card-body mb-3">
-//         <div class="row">
-//           <div class="col-md-3">
-//             <img class="img-fluid mb-2" src="${user.avatar_url}">
-//             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
-//           </div>
-//           <div class="col-md-9">
-//             <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-//             <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-//             <span class="badge badge-success">Followers: ${user.followers}</span>
-//             <span class="badge badge-info">Following: ${user.following}</span>
-//             <br><br>
-//             <ul class="list-group">
-//               <li class="list-group-item">Company: ${user.company}</li>
-//               <li class="list-group-item">Website/Blog: ${user.blog}</li>
-//               <li class="list-group-item">Location: ${user.location}</li>
-//               <li class="list-group-item">Member Since: ${user.created_at}</li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//       <h3 class="page-heading mb-3">Latest Repos</h3>
-//       <div id="repos"></div>
-//     `;
-//   }
-
-//   // Show user repos
-//   showRepos(repos) {
-//     let output = '';
-
-//     repos.forEach(function(repo) {
-//       output += `
-//         <div class="card card-body mb-2">
-//           <div class="row">
-//             <div class="col-md-6">
-//               <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-//             </div>
-//             <div class="col-md-6">
-//             <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
-//             <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
-//             <span class="badge badge-success">Forks: ${repo.forms_count}</span>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//     });
-
-//     // Output repos
-//     document.getElementById('repos').innerHTML = output;
-//   }
-
-//   // Show alert message
-//   showAlert(message, className) {
-//     // Clear any remaining alerts
-//     this.clearAlert();
-//     // Create div
-//     const div  =  document.createElement('div');
-//     // Add classes
-//     div.className = className;
-//     // Add text
-//     div.appendChild(document.createTextNode(message));
-//     // Get parent
-//     const container =  document.querySelector('.searchContainer');
-//     // Get search box
-//     const search = document.querySelector('.search');
-//     // Insert alert
-//     container.insertBefore(div, search);
-
-//     // Timeout after 3 sec
-//     setTimeout(() => {
-//       this.clearAlert();
-//     }, 3000);
-//   }
-
-//   // Clear alert message
-//   clearAlert() {
-//     const currentAlert = document.querySelector('.alert');
-
-//     if(currentAlert){
-//       currentAlert.remove();
-//     }
-//   }
-
-//   // Clear profile
-//   clearProfile() {
-//     this.profile.innerHTML = '';
-//   }
-// }
-
 class UI{
   constructor(){
     this.profile = document.getElementById('profile');
@@ -115,6 +18,7 @@ class UI{
             <span class="badge badge-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
+              <li class="list-group-item">Name: ${user.name}</li>
               <li class="list-group-item">Company: ${user.company}</li>
               <li class="list-group-item">Website/Blog: ${user.blog}</li>
               <li class="list-group-item">Location: ${user.location}</li>
@@ -126,5 +30,62 @@ class UI{
       <h3 class="page-heading mb-3">Latest Repos</h3>
       <div id="repos"></div>
     `;
+  }
+
+    // Show user repos
+    showRepos(repos) {
+      let output = '';
+  
+      repos.forEach(function(repo) {
+        output += `
+          <div class="card card-body mb-2">
+            <div class="row">
+              <div class="col-md-6">
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+              </div>
+              <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+  
+      // Output repos
+      document.getElementById('repos').innerHTML = output;
+    }
+
+  //Show alert when there is no such user
+  showAlert(message, classname){
+    this.clearAlert();
+    const div = document.createElement('div');
+    div.className = classname;
+    const messageTxt = document.createTextNode(message);
+    div.appendChild(messageTxt);
+    
+    //Append div above Search bar
+    const container = document.querySelector('.searchContainer');
+    const search = document.querySelector('.search');
+    
+    container.insertBefore(div, search);
+
+    setTimeout(()=>{
+      this.clearAlert();
+    }, 3000);
+  }
+  
+  //Clear Alert
+  clearAlert(){
+    const currentAlert = document.querySelector(".alert");
+    if(currentAlert){
+      currentAlert.remove();
+    }
+  }
+
+  //Clear profile
+  clearProfile(){
+    this.profile.innerHTML = '';
   }
 }
